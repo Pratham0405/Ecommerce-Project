c7c57edfe88bbe0f8479c07f7b4d540ec75010c4
Pratham Yeshwante <148319246+Pratham0405@users.noreply.github.com>
2024-03-03 14:15:05 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle5 as pickle
+
+file = open('pipe2.pkl', 'rb')
+model = pickle.load(file)
+file.close()
+
+
+data = pd.read_csv("traineddata.csv")
+
+#data['IPS'].unique()
+
+st.title("Yearly Amount Spent")
+ 
+# Average Session Length
+
+ASL = st.number_input('Average Session Length')
+
+
+
+# Time on App
+
+TOA = st.number_input('Time on App')
+
+# Time on Website
+
+TOW = st.number_input('Time on Website')
+
+# Length of Membership
+
+LOM = st.number_input('Length of Membership (in months)')
+
+
+
+
+query = np.array([ASL, TOA, TOW, LOM])
+
+query = query.reshape(1, 4)
+
+prediction = int(np.exp(model.predict(query)[0]))
+
+st.title("Predicted Yearly Amount spent by customer is" +
+             str(prediction-50)+"₹" + " to " + str(prediction+50)+"₹")
