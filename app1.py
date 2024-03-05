0c95e2f043004f014a94b7027538d35cc3a04d9a
Pratham Yeshwante <148319246+Pratham0405@users.noreply.github.com>
2024-03-05 14:25:42 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,13 +12,13 @@ import joblib
 model = joblib.load('pipe4.pkl')
 
 # Define the function to make predictions
-def predict(years_of_membership, avg_session_length, time_on_app, time_on_website):
+def predict(YAS, ASL, TOA, TOW):
     # Prepare the input data as a dictionary
     input_data = {
-        'Length of Membership': years_of_membership,
-        'Avg. Session Length': avg_session_length,
-        'Time on App': time_on_app,
-        'Time on Website': time_on_website
+        'Length of Membership': YAS,
+        'Avg. Session Length': ASL,
+        'Time on App': TOA,
+        'Time on Website': TOW
     }
     
     # Convert the input data to a pandas DataFrame
@@ -36,15 +36,15 @@ def main():
     
     # Add input fields for user input
     st.sidebar.header('User Input')
-    years_of_membership = st.sidebar.number_input('Years of Membership', min_value=0, max_value=10, step=1)
-    avg_session_length = st.sidebar.number_input('Average Session Length (minutes)', min_value=0.0, max_value=60.0, step=0.1)
-    time_on_app = st.sidebar.number_input('Time on App (minutes)', min_value=0.0, max_value=24.0, step=0.1)
-    time_on_website = st.sidebar.number_input('Time on Website (minutes)', min_value=0.0, max_value=24.0, step=0.1)
+    YAS = st.sidebar.number_input('Years of Membership', min_value=0, max_value=10, step=1)
+    ASL = st.sidebar.number_input('Average Session Length (minutes)', min_value=0.0, max_value=60.0, step=0.1)
+    TOA = st.sidebar.number_input('Time on App (minutes)', min_value=0.0, max_value=24.0, step=0.1)
+    TOW = st.sidebar.number_input('Time on Website (minutes)', min_value=0.0, max_value=24.0, step=0.1)
     
     # Add a button to trigger predictions
     if st.sidebar.button('Predict'):
         # Call the predict function with user inputs
-        prediction = predict(years_of_membership, avg_session_length, time_on_app, time_on_website)
+        prediction = predict(YAS, ASL, TOA, TOW)
         
         # Display the prediction
         st.success(f'The predicted annual income is ${prediction:.2f}')
