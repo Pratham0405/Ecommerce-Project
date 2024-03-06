48b9c2417b852cf805f5fe5c9bc5cec96664d989
Pratham Yeshwante <148319246+Pratham0405@users.noreply.github.com>
2024-03-06 12:50:58 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,12 +8,13 @@ import joblib
 model = joblib.load("pipe4.pkl")
 
 # Define the function to make predictions
-def predict(length_of_membership, avg_session_length, time_on_app, time_on_website):
+def predict(avg_session_length, time_on_app, time_on_website,length_of_membership):
     input_data = pd.DataFrame({
-        'LOM': [length_of_membership],
+        
         'ASL': [avg_session_length],
         'TOA': [time_on_app],
         'TOW': [time_on_website]
+        'LOM': [length_of_membership],
     })
     prediction = model.predict(input_data)
     return prediction[0]
@@ -24,14 +25,14 @@ def main():
     st.write("This app predicts the yearly amount spent by a customer based on their input.")
 
     # Add input fields for user data
-    length_of_membership = st.slider("Years of Membership", 0, 10, 1)
     avg_session_length = st.slider("Avg. Session Length (minutes)", 0.0, 60.0, 30.0)
     time_on_app = st.slider("Time on App (minutes)", 0.0, 120.0, 60.0)
     time_on_website = st.slider("Time on Website (minutes)", 0.0, 120.0, 60.0)
+    length_of_membership = st.slider("Years of Membership", 0, 10, 1)
 
     # Add a button to make predictions
     if st.button("Predict"):
-        prediction = predict(length_of_membership, avg_session_length, time_on_app, time_on_website)
+        prediction = predict(avg_session_length, time_on_app, time_on_website,length_of_membership)
         st.success(f"The predicted yearly amount spent is: ${prediction:.2f}")
 
 # Run the app
